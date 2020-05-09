5aad1448f1711ea96b6f5f1c6461ce49abcbbb15
Morgan <morgan.brehinier@gmail.com>
2020-05-09 14:20:14 +0200

    small adjustments

--- app/views/tracker/torrents.php
+++ app/views/tracker/torrents.php
@@ -1,29 +1,25 @@
 <?php require_once APP_ROUTE . '/views/inc/navbar_header.php';?>
 
-<div id="home_content" class="container">
-    <div style="padding: 20px;">
-        <div style="padding: 20px 0;">
-            Search Will be Here
-        </div>
-        <table class="table table-bordered table-striped table-hover">
-            <thead>
-                <tr class="">
-                    <th>Type</th>
-                    <th>Name</th>
-                    <th>Com.</th>
-                    <th>Time.</th>
-                    <th>Size.</th>
-                    <th>Seeds</th>
-                    <th>Leech</th>
-                    <th>Down</th>
-                    <th>Uploader</th>
-                </tr>
-            </thead>
-            <tbody>
-                <?php echo $data['showList']; ?>
-            </tbody>
-        </table>
-    </div>
+<div style="padding: 20px 0;">
+    Search Will be Here
 </div>
+<table class="torrenttable">
+    <thead>
+        <tr class="">
+            <th>Type</th>
+            <th>Name</th>
+            <th>Com.</th>
+            <th>Time.</th>
+            <th>Size.</th>
+            <th>Seeds</th>
+            <th>Leech</th>
+            <th>Down</th>
+            <th>Uploader</th>
+        </tr>
+    </thead>
+    <tbody>
+        <?php echo $data['showList']; ?>
+    </tbody>
+</table>
 
 <?php require_once APP_ROUTE . '/views/inc/navbar_footer.php';?>
